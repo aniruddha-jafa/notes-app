e24157b2dcdf251e890758a9c8b204ee3000a635
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-01-08 19:25:00 +0530

    Basic read & validation of form data

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -4,14 +4,27 @@
 
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
 
 router.get('/note/create', (req, res) => {
   res.render('note', {})
 })
 
-router.post('/note/create', (req, res) => {
-  res.send('to implement: submit note')
+router.post('/note/create',
+  body('title').trim().escape(),
+  body('title').not().isEmpty(),
+  body('title').isLength({ min: 2 }),
+  body('note').trim().escape(),
+  body('note').not().isEmpty(),
+  body('note').isLength({ min: 3 }),
+(req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+  res.send('Successful submit!')
 })
 
 
