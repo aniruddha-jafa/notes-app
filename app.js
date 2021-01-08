062be0287e2e9780fbf3d5acefd818b00ab73395
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-01-08 17:54:52 +0530

    Added hbs view engine, defined basic partials for home & create page (no form submit yet)

--- app.js
+++ app.js
@@ -6,17 +6,22 @@ const app = express()
 
 const path = require('path')
 const viewsPath = path.join(__dirname, '/views')
+const partialsPath = path.join(viewsPath, '/partials')
+
 app.use(express.static(viewsPath))
 
+const hbs = require('hbs')
+hbs.registerPartials(partialsPath)
+
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.render('home', {})
 })
 
-app.post('/', (req, res) => {
-  console.log('Submission detected, redirecting...')
-  res.redirect('/')
+app.get('/create', (req, res) => {
+  res.render('create-note', {})
 })
-
 
 const portToUse = 3000 || 3001
 app.listen(portToUse, () => {
