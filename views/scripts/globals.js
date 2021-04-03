e2c02cca87a279ebf41466f583ddb2fa9a6c95e4
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-04-03 15:58:01 +0530

    Extended quill.js editor options

--- views/scripts/globals.js
+++ views/scripts/globals.js
@@ -1,22 +1,25 @@
 'use strict'
 
 // intended to be in shared scope for all scripts
-let isNewNote = true
-const API_URL = '/api/notes'
-
-const quillEditorSelector = '#quill-editor'
-const editorContainerSelector = '#editor-container'
-const noteFormSelector = '#note-form'
-const notesListContainerSelector = '#notes-list-container'
-
-const editorOptions = {
-  theme: 'snow',
-  modules: {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline'],
-      ['image', 'code-block']
-    ]
-  },
-  placeholder: 'Compose an epic...'
+const globals = {
+  isNewNote: true,
+  API_URL: '/api/notes',
+  currentNoteId: null,
+  quillEditorSelector: '#quill-editor',
+  editorContainerSelector: '#editor-container',
+  noteFormSelector: '#note-form',
+  notesListContainerSelector: '#notes-list-container',
+  editorOptions: {
+    theme: 'snow',
+    modules: {
+      toolbar: [
+        [{ 'header': 1 }, { 'header': 2 }],
+        ['bold', 'italic', 'underline', 'strike', { 'align': [] }],
+        [{ 'list': 'bullet' }, {'list': 'ordered'}, 'blockquote'],
+        ['link', 'image', 'code-block'],
+        [{ 'color': [] }, { 'background': [] }, { 'font': [] }],
+      ]
+    },
+    placeholder: 'Write here...'
+  }
 }
