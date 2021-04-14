547a482c0d8872f4f33a6ee3472c8b3933c44645
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-04-14 15:28:31 +0530

    Refactored app.js to improve readability

--- app.js
+++ app.js
@@ -5,30 +5,30 @@
 const express = require('express')
 const app = express()
 
-// config
-require('dotenv').config()
-
 // libraries
 const path = require('path')
 const hbs = require('hbs')
+
+// module imports
+const notesRouter = require('./routes/noteRoutes')
 
 // paths
 const viewsPath = path.join(__dirname, '/views')
 const partialsPath = path.join(viewsPath, '/partials')
 
-// module imports
-const notesRouter = require('./routes/noteRoutes')
+// config
+require('dotenv').config()
+app.set('view engine', 'hbs')
+app.set('strict routing', true)
 
 // view engine & static assets
 app.use(express.static(viewsPath))
 hbs.registerPartials(partialsPath)
-app.set('view engine', 'hbs')
 
 // routes
 app.use('/', notesRouter)
 
 const port = process.env.PORT || 8000
-
 app.listen(port, () => {
   console.log(`Listening at port ${port}...`)
 })
