c56b2759a8fc5be1e39c100a88f2e20376420469
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-05-24 12:51:23 +0530

    Use quilljs from local source instead of CDN

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // libraries
 const express = require('express')
 const path = require('path')
-const ejs = require('ejs')
+// const ejs = require('ejs')
 
 // app
 const app = express()
@@ -10,7 +10,8 @@ const app = express()
 const notesRouter = require('./routes/noteRoutes')
 
 // paths
-const viewsPath = path.join(__dirname, '/views')
+const viewsPath = path.join(__dirname, 'views')
+const quilljsPath = path.join('node_modules', 'quill', 'dist')
 
 // config
 require('dotenv').config()
@@ -20,9 +21,12 @@ app.set('views', viewsPath)
 
 app.use('/api', express.json())
 
-// view engine & static assets
+// view engine
 app.set('view engine', 'ejs')
+
+// static assets
 app.use(express.static('public'))
+app.use('/vendor-assets/quill', express.static(quilljsPath))
 
 // routes
 app.use('/', notesRouter)
