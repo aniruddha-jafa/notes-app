92e5568591ca1e0c13157230f922b733541311d5
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-05-24 09:58:23 +0530

    Refactored models/note.j

--- src/models/note.js
+++ src/models/note.js
@@ -1,25 +1,23 @@
-'use strict'
-
 require('dotenv').config()
 
 const mongoose = require('mongoose')
 
 const NoteSchema = new mongoose.Schema({
   title: {
-          type: String,
-          maxlength: [ 50,'Title must note exceed 50 characters'],
-          trim: true,
-          default: 'Untitled'
-        },
+    type: String,
+    maxlength: [50, 'Title must note exceed 50 characters'],
+    trim: true,
+    default: 'Untitled',
+  },
   date: {
-          type: Date,
-          required: true,
-        },
+    type: Date,
+    required: true,
+  },
   body: {
-          type: mongoose.Schema.Types.Mixed,
-          required: true,
-          trim: true
-        }
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+    trim: true,
+  },
 })
 
 module.exports = mongoose.model('Note', NoteSchema)
