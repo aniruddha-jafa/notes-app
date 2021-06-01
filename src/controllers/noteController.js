7376a161a3c81452effd16a25dc8edb86e708957
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-06-01 09:27:05 +0530

    Fixed linting erros after git merge

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -1,106 +1,89 @@
-'use strict';
+'use strict'
 
 // libraries
 const jsonParser = require('express').json()
-const ejs = require('ejs')
-const path = require('path')
+const debug = require('debug')('http')
 
 // modules
 const Note = require('../models/note')
 
-
-const noteScriptsPath = path.join(__dirname, '..', '/views', '/scripts')
-
-
 exports.getHome = async function (req, res, next) {
   try {
-      //res.render('helloworld')
-      console.log('noteScriptsPath', noteScriptsPath)
-
-      res.render('home', {
-                      scriptsPath: noteScriptsPath,
-                      globalsPath: path.join(noteScriptsPath, '/globals.js')
-                    })
-    } catch(err) {
-      next(err)
-    }
+    res.render('home', {})
+  } catch (err) {
+    next(err)
   }
-
+}
 
 exports.notesReadOne = async function (req, res, next) {
   try {
-    console.log('Received GET request for a single note')
+    debug('Received GET request for a single note')
     const note = await Note.findById(req.params.id)
     if (!note) {
       throw new Error('Note not found')
     }
     res.json(note)
     next()
-  } catch(err) {
+  } catch (err) {
     next(err)
   }
 }
 
-
-// READ all notes
-let LIMIT = 5, skip = 0
+const LIMIT = 5
+let skip = 0
 exports.notesReadMany = async function (req, res, next) {
   try {
-    console.log('Received GET request for multiple notes')
+    debug('Received GET request for multiple notes')
     const notes = await Note.find({}).skip(skip).limit(LIMIT)
     res.json(notes)
-    console.log(`Sent ${notes.length} notes`)
+    debug(`Sent ${notes.length} notes`)
     skip += LIMIT
     next()
-
-  } catch(err) {
+  } catch (err) {
     next(err)
   }
-
 }
 
 exports.notesCreateOne = [
   jsonParser,
-  async function (req, res, next)  {
-   try {
-     let formData = await req.body
-     console.log('POST request for formData:', formData)
-     let note = new Note(formData)
-     note = await note.save()
-     res.json({ _id: note._id })
-     next()
-   } catch(err) {
-   next(err)
- }
-}]
-
+  async function (req, res, next) {
+    try {
+      const formData = await req.body
+      debug('POST request for formData:', formData)
+      let note = new Note(formData)
+      note = await note.save()
+      res.json({ _id: note._id })
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }]
 
 exports.notesUpdateOne = [
   jsonParser,
-  async function(req, res, next) {
-  try {
-    let formData = await req.body
-    console.log('PUT request for note:', formData)
-    formData.body = await JSON.parse(formData.body)
-    await Note.findByIdAndUpdate(req.params.id, formData)
-    res.json({ message: 'updated' })
-    next()
-
-  } catch (err) {
-    next(err)
-  }
-}]
+  async function (req, res, next) {
+    try {
+      const formData = await req.body
+      debug('PUT request for note:', formData)
+      formData.body = await JSON.parse(formData.body)
+      await Note.findByIdAndUpdate(req.params.id, formData)
+      res.json({ message: 'updated' })
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }]
 
 exports.notesDeleteOne = [
   jsonParser,
-  async function(req, res, next) {
-  try {
-    const noteId = await req.params.id
-    console.log('DELETE request for note:', noteId)
-    //await Note.findByIdAndDelete(noteId)
-    res.json({ message: 'deleted' })
-  } catch (err) {
-    next(err)
-  }
-}
+  async function (req, res, next) {
+    try {
+      const noteId = await req.params.id
+      debug('DELETE request for note:', noteId)
+      // await Note.findByIdAndDelete(noteId)
+      res.json({ message: 'deleted' })
+    } catch (err) {
+      next(err)
+    }
+  },
 ]
--- src/models/note.js
+++ src/models/note.js
@@ -3,30 +3,36 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
-mongoose.set('useNewUrlParser', true)
-mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true)
-mongoose.set('useUnifiedTopology', true)
 
-const notesDbConnection = mongoose.createConnection(process.env.MONGODB_NOTES_URI)
+const connectionOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+}
+
+const notesDbConnection = mongoose.createConnection(
+  process.env.MONGODB_NOTES_URI,
+  connectionOptions,
+)
 
 const NoteSchema = new mongoose.Schema({
   title: {
-          type: String,
-          maxlength: [ 50,'Title must note exceed 50 characters'],
-          trim: true,
-          default: 'Untitled'
-        },
+    type: String,
+    minlength: [2, 'Title must contain at least 2 characters'],
+    maxlength: [50, 'Title must note exceed 50 characters'],
+    trim: true,
+    default: 'Untitled',
+  },
   date: {
-          type: Date,
-          required: true,
-        },
+    type: Date,
+    required: true,
+  },
   body: {
-          type: mongoose.Schema.Types.Mixed,
-          required: true,
-          trim: true
-        }
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+    trim: true,
+  },
 })
-
 
 module.exports = notesDbConnection.model('Note', NoteSchema)
